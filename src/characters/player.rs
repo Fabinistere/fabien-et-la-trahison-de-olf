32ff33990d0be79a37da6b423ba5b55dba57fc9c
Wabtey <wabtey@disroot.org>
2024-06-21 12:42:50 +0200

    remove fixme comment (Player not freezed when there is a dialog)
    it was already fixed by `.add_systems(Update, (player_movement.run_if(hud_closed)))`

--- src/characters/player.rs
+++ src/characters/player.rs
@@ -47,7 +47,6 @@ pub struct PlayerInteractionSensor;
 #[derive(Component)]
 pub struct PlayerCloseSensor;
 
-/// FIXME: Freeze the player when in dialog (trigger for ex when interacting while running)
 fn player_movement(
     key_bindings: Res<KeyBindings>,
     keyboard_input: Res<Input<KeyCode>>,
